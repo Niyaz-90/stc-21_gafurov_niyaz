6a1469a93113aeaac2ac8d96e2dcd5182fc6b914
Niyaz-90 <niyazga-12345@mail.ru>
2021-03-24 23:36:53 +0300

    finish HW6

--- Homework6/src/main/java/ru/inno/hw6/task2/Generator.java
+++ Homework6/src/main/java/ru/inno/hw6/task2/Generator.java
@@ -1,36 +1,26 @@
 package ru.inno.hw6.task2;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Generator {
     private static Random random = new Random();
-//    private File file;
-    private Writer writer;
     private final char[] punctMarksArray = {'.', '!', '?'};  // как понять (.|!|?)+" ".   + и "" тоже считаются?
 
     public Generator() {
-//        this.file = new File("newFile.txt");
-//        try {
-//            this.writer = new FileWriter(file);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     public String generateWord() {
 
-        int wordSize = random.nextInt(16);
+        int wordSize = random.nextInt(16) + 1;
         StringBuilder sb = new StringBuilder("");
         for (int i = 0; i < wordSize; i++) {
-            sb.append((char)(random.nextInt(26) + 65));
+            sb.append((char) (random.nextInt(26) + 65));
         }
         return String.valueOf(sb).toLowerCase();
     }
 
-    // TODO: 22.03.2021 заменить все wordsArray на words(из параметров), разобраться с вероятностью
+
     public void getFiles(String path, int n, int size, String[] words, int probability) throws IOException {
         int filesCount = 0;
         while (filesCount != n) {
@@ -38,18 +28,17 @@ public class Generator {
             dir.mkdir();
             File file = new File(path, "resultFile" + filesCount + ".txt");
             file.createNewFile();
-            try (Writer writer = new FileWriter(file)) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
                 StringBuilder sentenceBuilder = new StringBuilder();
-                StringBuilder wordBuilder = new StringBuilder(" ");
-
+                StringBuilder wordBuilder = new StringBuilder();
 
                 //пока не достигли требуемой длины файла
                 while (file.length() <= size) {
-                    int paragraphSize = random.nextInt(20) + 1;
+                    int paragraphSize = random.nextInt(21) + 1;
 
                     //пока не достигли требуемой длины абзаца
                     for (int i = 0; i < paragraphSize; i++) {
-                        int sentenceSize = random.nextInt(16);
+                        int sentenceSize = random.nextInt(16) + 1;
 
                         // флаг - содержит предложение запятую или нет
                         boolean containsComma = random.nextBoolean();
@@ -57,16 +46,12 @@ public class Generator {
                         // флаг - удовлетворяет условию вероятности(слово из массива)
                         boolean containsArraysWord = Math.random() <= 1.0 / probability;
 
-
-                        // <<<---
-
                         // пока не достигли требуемой длины предложения
                         for (int j = 0; j < sentenceSize; j++) {
 
                             // если попали в диапазон вероятности
                             if (containsArraysWord) {
                                 wordBuilder.append(" " + words[random.nextInt(words.length)]);
-                                continue;
                             }
 
                             // положение запятой в предложении
@@ -75,14 +60,21 @@ public class Generator {
                             // генерируем слово из списка
                             wordBuilder.append(" " + generateWord());
 
+                            if (sentenceSize == 1) {
+                                wordBuilder.setCharAt(1, Character.toUpperCase(wordBuilder.charAt(1)));
+                                wordBuilder.append(punctMarksArray[random.nextInt(punctMarksArray.length)]);
+                                wordBuilder.setLength(0);
+                                continue;
+                            }
 
                             //если оно первое, то первую букву делаем заглавной
-                            if (j == 0) {
+                            else if (j == 0) {
                                 wordBuilder.setCharAt(1, Character.toUpperCase(wordBuilder.charAt(1)));
                             }
+
                             // если последнее, то поставить точку. Со switch - case не получается т.к. нужна константа
                             else if (j == sentenceSize - 1) {
-                                wordBuilder.append(random.nextInt(punctMarksArray.length));
+                                wordBuilder.append(punctMarksArray[random.nextInt(punctMarksArray.length)]);
                             }
 
                             // если предложение содержит запятую - поставить её
@@ -92,20 +84,20 @@ public class Generator {
 
                             // добавляем слово к предложению
                             sentenceBuilder.append(wordBuilder);
+                            wordBuilder.setLength(0);
 
                         }
-                        writer.write(String.valueOf(sentenceBuilder));
+
+                        writer.write(sentenceBuilder.toString());
+
                         sentenceBuilder.setLength(0);
                     }
-//                    if (int i < paragraphSize - 1) {
-                        writer.write("\r\n -->");
-//                    }
 
-                    // перенос каретки и абзац
+                    writer.write("\r\n");
+
                 }
 
                 filesCount++;
-
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -114,11 +106,5 @@ public class Generator {
 
     }
 
-    public void generateReadyFile() {
 
-    }
-
-    public void generateLastWord(int size) {
-
-    }
 }
