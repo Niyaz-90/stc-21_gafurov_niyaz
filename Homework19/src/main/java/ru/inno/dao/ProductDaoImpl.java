d3d76a3afdec8b0f0285865537945408aaded8ff
Niyaz-90 <niyazga-12345@mail.ru>
2021-05-02 15:34:14 +0300

    add create() of ProductDaoImpl hw19

--- Homework19/src/main/java/ru/inno/dao/ProductDaoImpl.java
+++ Homework19/src/main/java/ru/inno/dao/ProductDaoImpl.java
@@ -5,13 +5,15 @@ import ru.inno.model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ProductDaoImpl implements ProductDao {
     //SQL
     private static final String insertQuery = "INSERT INTO products(product_name, cost) VALUES (?, ?);";
     //SQL
-    private static final String updateByIdQuery = "UPDATE products(product_name, cost) SET (?, ?);";
+    private static final String updateByIdQuery =
+            "UPDATE products(product_name, cost) SET (?, ?) WHERE product_id = ?;";
     //SQL
     private static final String deleteByIdQuery = "DELETE FROM products WHERE product_id = ?;";
 
@@ -31,12 +33,31 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public void updateById(int productId, String name, int cost) {
-
+        try(Connection connection = ConnectionManager.getConnection();
+        PreparedStatement ps = connection.prepareStatement(updateByIdQuery)){
+            connection.setAutoCommit(false);
+            ps.setString(1, name);
+            ps.setInt(2, cost);
+            ps.setInt(3, productId);
+            System.out.println(ps.executeUpdate() + " records updated.");
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteById(int productId) {
-
+        try(Connection connection = ConnectionManager.getConnection();
+        PreparedStatement ps = connection.prepareStatement(deleteByIdQuery)){
+            connection.setAutoCommit(false);
+            ps.setInt(1, productId);
+            ResultSet rs = ps.executeQuery();
+            System.out.println("Product: " + rs.getString(2) + "(id: " + productId + " ) deleted");
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
