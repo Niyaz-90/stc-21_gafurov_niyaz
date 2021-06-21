5577982d652dfe690fe69c4467b1f290bc6c9447
Niyaz-90 <niyazga-12345@mail.ru>
2021-06-21 23:56:09 +0300

    after demo

--- Spring mvc/REST_service/src/main/java/ru/inno/controller/PrivateAccessController.java
+++ Spring mvc/REST_service/src/main/java/ru/inno/controller/PrivateAccessController.java
@@ -1,11 +1,9 @@
 package ru.inno.controller;
 
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.inno.entity.Card;
 import ru.inno.service.UserService;
@@ -19,15 +17,10 @@ public class PrivateAccessController {
     private UserService userService;
 
     @PostMapping(value = "/pay", produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ResponseBody
     public ResponseEntity<String> pay(@RequestBody Card card){
-        System.out.println("1");
         userService.doTransaction();
-        System.out.println("1");
         Random random = new Random();
-        System.out.println("1");
         boolean success = random.nextBoolean();
-        System.out.println("1");
         if(success){
             System.out.println("Успешно");
             return new  ResponseEntity<>("Успешно!", HttpStatus.ACCEPTED);
